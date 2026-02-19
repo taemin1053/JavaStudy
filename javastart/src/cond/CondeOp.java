657734ae76d24377e51d57a55565d921b84b34b9
jtmexs1218 <jtmexs1218@gmail.com>
2026-02-19 17:42:47 +0900

    fix: 잘못 병합된 javastart 삭제 및 원상복구

--- javastart/src/cond/CondeOp.java
+++
@@ -1,9 +0,0 @@
-package cond;
-//삼항 연산자
-public class CondeOp {
-    public static void main(String[] args) {
-        int age = 18;
-        String status = (age >= 18) ? "성인" : "미성년자";
-        System.out.println(status);
-    }
-}
--- javastart/src/variable/Var1.java
+++
@@ -1,9 +0,0 @@
-package variable;
-//package 자바파일을 구분하는 진구
-public class Var1 {
-    public static void main(String[] args) {
-        System.out.println(20);
-        System.out.println(20);
-        System.out.println(20);
-    }
-}
