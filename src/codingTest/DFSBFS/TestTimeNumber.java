6cdf02fe7bb154454de881aeb621051eef59a700
jtmexs1218 <jtmexs1218@gmail.com>
2026-09-11 17:06:41 +0900

    timeNumber coding test

--- src/codingTest/DFSBFS/TestTimeNumber.java
+++ src/codingTest/DFSBFS/TestTimeNumber.java
@@ -1,4 +1,39 @@
 package codingTest.DFSBFS;
+class Counter{
+    private final int[] number;
+    private int count= 0;
+    private int sum = 0;
+    public Counter(int[] number){
+        this.number = number;
+    }
+    private  void dfs(int[] number, int target, int index, int sum){
+        if(index == number.length) {
+            if (sum == target) {
+                count++;
 
+            }
+            return;
+        }
+        dfs(number , target, index +1 , sum + number[index]);
+        dfs(number , target, index +1 , sum - number[index]);
+
+    }
+    public int getCount(int target){
+        dfs(number, target, 0,0);
+        return count;
+    }
+}
 public class TestTimeNumber {
+    public int solution(int[] numbers, int target) {
+        Counter counter = new Counter(numbers);
+        return counter.getCount(target);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 1, 2, 1};
+        int b = 4;
+        TestTimeNumber ts = new TestTimeNumber();
+        System.out.println(ts.solution(a,b));
+
+    }
 }
